fe1cc90161bc9fb8eb7caf7c0840ca6b069872a9
TayoDavid <omotayo4david@gmail.com>
2022-02-18 12:48:54 +0100

    Updated the sandwich detail screen

--- Shared/SandwichDetail.swift
+++ Shared/SandwichDetail.swift
@@ -12,18 +12,42 @@ struct SandwichDetail: View {
     @State private var zoomed = false
     
     var body: some View {
-        Image(sandwich.imageName)
-            .resizable()
-            .aspectRatio(contentMode: zoomed ? .fill : .fit)
-            .navigationTitle(sandwich.name)
-            .onTapGesture { zoomed.toggle() }
+        VStack {
+            Spacer(minLength: 0)
+            
+            Image(sandwich.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation {
+                        zoomed.toggle()
+                    }
+                }
+            
+            Spacer(minLength: 0)
+            
+            if sandwich.isSpicy && !zoomed {
+                HStack {
+                    Spacer()
+                    Label("Spicy", systemImage: "flame.fill")
+                    Spacer()
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .font(.headline.smallCaps())
+                .background(.red)
+                .foregroundColor(.yellow)
+                .transition(.move(edge: .bottom))
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .navigationTitle(sandwich.name)
     }
 }
 
 struct SandwichDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            SandwichDetail(sandwich: testData[0])
+            SandwichDetail(sandwich: testData[1])
         }
      }
 }
